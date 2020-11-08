536ef96e3165a894c144f8c2ccc84265960fa95a
Amol Saini <38129418+amolsr@users.noreply.github.com>
2020-11-08 12:51:20 +0530

    Delete DBIntializer.java

--- src/com/tnt/dao/DBIntializer.java
+++
@@ -1,8 +0,0 @@
-package com.tnt.dao;
-
-public interface DBIntializer {
-	String DRIVER = "com.mysql.cj.jdbc.Driver";
-	String CON_STRING = System.getenv("DATABASE_URL");
-	String USERNAME = System.getenv("DBUSER");
-	String PASSWORD = System.getenv("DBPASS")";
-}
