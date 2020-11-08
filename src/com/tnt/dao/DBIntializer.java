488c08cc8047fabf1fd9c5191f031eb8c4986e96
Amol Saini <38129418+amolsr@users.noreply.github.com>
2020-11-08 12:38:49 +0530

    Update DBIntializer.java

--- src/com/tnt/dao/DBIntializer.java
+++ src/com/tnt/dao/DBIntializer.java
@@ -2,7 +2,7 @@ package com.tnt.dao;
 
 public interface DBIntializer {
 	String DRIVER = "com.mysql.cj.jdbc.Driver";
-	String CON_STRING = "jdbc:mysql://localhost:3306/tourNtravel";
-	String USERNAME = "root";
-	String PASSWORD = "toor";
+	String CON_STRING = System.getenv("DATABASE_URL");
+	String USERNAME = System.getenv("DBUSER");
+	String PASSWORD = System.getenv("DBPASS")";
 }
