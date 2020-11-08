798355cc7f007117f82fb5c86f82466b3198b924
Amol Saini <38129418+amolsr@users.noreply.github.com>
2020-11-08 12:47:32 +0530

    Update DBManager.java

--- src/com/tnt/dao/DBManager.java
+++ src/com/tnt/dao/DBManager.java
@@ -12,12 +12,11 @@ public class DBManager {
 	private static Connection con;
 
 	public static Connection getcon() throws SQLException {
-//		try {
-//			Class.forName(DBIntializer.DRIVER);
-//			con = DriverManager.getConnection(DBIntializer.CON_STRING, DBIntializer.USERNAME, DBIntializer.PASSWORD);
-//		} catch (Exception e) {
-//			System.out.println(e);
-//		}
+		try {
+			con = DriverManager.getConnection(DBIntializer.CON_STRING, DBIntializer.USERNAME, DBIntializer.PASSWORD);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		return con;
 	}
 
